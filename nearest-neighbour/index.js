3d0844ec4b3f79fcc4fd256d739f4826dc8d4423
Diego Barbaresco <diego.bar@live.it>
2024-08-04 23:52:16 +0200

    algorithm is working

--- nearest-neighbour/index.js
+++ nearest-neighbour/index.js
@@ -1,120 +1,47 @@
 import Point from "./Point.js";
 import pointsFactory from "./pointsFactory.js";
 
-const points = pointsFactory();
-const processedPoints = [];
+const startingPoint = pointsFactory()[0];
+let processedIds = [startingPoint.id];
+nearestNeighbourOf(startingPoint);
+// const nearestNeighbour = nearestNeighbourOf(startingPoint);
+// console.log(`The neighbour of ${startingPoint.id} is ${nearestNeighbour.id}`);
+// console.log(`Processed ids are ${processedIds.join(", ")}`);
 
-points.forEach((p) => {
-  console.log("processedPoints", processedPoints);
+function nearestNeighbourOf(theStartingPoint, recursive = true) {
+  let nearestNeighbour = theStartingPoint;
+  let distanceAcc = 9999;
 
-  findNearestNeighbour(p);
-});
+  pointsFactory()
+    .filter(excludeStartingPoint)
+    .forEach((p) => {
+      if (distanceAcc > temporaryPoint().dist) {
+        distanceAcc = temporaryPoint().dist;
+        nearestNeighbour = temporaryPoint();
+      }
 
-function findNearestNeighbour(startingPoint) {
-  processedPoints.push(startingPoint);
-  let nearestNeighbour = null;
-  points.filter(excludeStartingPoint).forEach((p) => {
-    const intermediateX = startingPoint.x - p.x;
-    const intermediateY = startingPoint.y - p.y;
-    const intermediatePoint = new Point(p.id, intermediateX, intermediateY);
+      function temporaryPoint() {
+        const intermediateX = theStartingPoint.x - p.x;
+        const intermediateY = theStartingPoint.y - p.y;
+        return new Point(p.id, intermediateX, intermediateY);
+      }
+    });
 
-    if (
-      !nearestNeighbour ||
-      (intermediatePoint.dist < nearestNeighbour.dist &&
-        !processedPoints.includes((x) => p.id == x.id))
-    ) {
-      nearestNeighbour = intermediatePoint;
-      processedPoints.push(p);
-    }
-  });
-
+  processedIds.push(nearestNeighbour.id);
   console.log(
-    `The nearest neighbour of ${startingPoint.id} is ${nearestNeighbour.id}`
+    `The neighbour of ${theStartingPoint.id} is ${nearestNeighbour.id}`
   );
 
+  if (processedIds.length < 10) {
+    const newStartingPoint = pointsFactory().find(
+      (x) => x.id == nearestNeighbour.id
+    );
+    nearestNeighbourOf(newStartingPoint, true);
+  }
+
   function excludeStartingPoint(aPoint) {
-    return aPoint.id != startingPoint.id;
+    return (
+      aPoint.id != theStartingPoint.id && !processedIds.includes(aPoint.id)
+    );
   }
 }
-
-// function findNearestNeighbour(startingPoint) {
-//   // let result = points
-//   //   .filter(excludeStartingPoint)
-//   //   .map(calculateDistanceFrom)
-//   //   .sort(byDistance);
-
-//   processedPoints.push(startingPoint);
-//   let nearestNeighbour = null;
-//   points.filter(excludeStartingPoint).forEach((p) => {
-//     const intermediateX = startingPoint.x - p.x;
-//     const intermediateY = startingPoint.y - p.y;
-//     const intermediatePoint = new Point(p.id, intermediateX, intermediateY);
-
-//     if (
-//       !nearestNeighbour ||
-//       (intermediatePoint.dist < nearestNeighbour.dist &&
-//         !processedPoints.includes((x) => p.id == x.id))
-//     ) {
-//       nearestNeighbour = intermediatePoint;
-//       processedPoints.push(p);
-//     }
-//   });
-
-//   console.log(
-//     `The nearest neighbour of ${startingPoint.id} is ${nearestNeighbour.id}`
-//   );
-
-//   function excludeStartingPoint(aPoint) {
-//     return aPoint.id != startingPoint.id;
-//   }
-//   // function calculateDistanceFrom(aPoint) {
-//   //   const intermediateX = startingPoint.x - aPoint.x;
-//   //   const intermediateY = startingPoint.y - aPoint.y;
-//   //   return new Point(aPoint.id, intermediateX, intermediateY);
-//   // }
-//   // function byDistance(a, b) {
-//   //   return a.dist - b.dist;
-//   // }
-// }
-// function nearest2(startingPoint, pointsArray) {
-//   // exclude the starting point from the points array
-//   let currentPoint = startingPoint;
-//   let remainingPoints = pointsArray.filter(excludeStartingPoint);
-
-//   // find the nearest point to the starting point
-//   let nearestNeighbour = null;
-
-//   remainingPoints.forEach((p) => {
-//     const intermediateX = startingPoint.x - p.x;
-//     const intermediateY = startingPoint.y - p.y;
-//     const intermediatePoint = new Point(p.id, intermediateX, intermediateY);
-
-//     if (!nearestNeighbour || intermediatePoint.dist < nearestNeighbour.dist) {
-//       nearestNeighbour = intermediatePoint;
-//     }
-//   });
-
-//   console.log(nearestNeighbour);
-//   currentPoint = nearestNeighbour;
-//   remainingPoints = remainingPoints.filter(excludeStartingPoint);
-//   nearest2(nearestNeighbour, remainingPoints);
-
-//   // exclude the nearest point that you just found from the points array
-
-//   function excludeStartingPoint(aPoint) {
-//     return aPoint.id != currentPoint.id;
-//   }
-// }
-// function nearest3(startingPoint) {
-//   let currentPoint = startingPoint;
-//   let remainingPoints = points.filter(excludeStartingPoint);
-
-//   points.forEach((p) => {
-//     currentPoint = p;
-//     remainingPoints = remainingPoints.filter(excludeStartingPoint);
-//   });
-
-//   function excludeStartingPoint(aPoint) {
-//     return aPoint.id != currentPoint.id;
-//   }
-// }
