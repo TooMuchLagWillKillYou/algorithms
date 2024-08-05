9283b25c82671a0e76b2935a65c703c4ed0e85a5
Diego Barbaresco <77250792+TooMuchLagWillKillYou@users.noreply.github.com>
2024-08-05 08:12:40 +0000

    pointsFactory is now a class

--- nearest-neighbour/PointsFactory.js
+++ nearest-neighbour/PointsFactory.js
@@ -1,13 +1,9 @@
 import Point from "./Point.js";
 
-function pointsFactory(random = false) {
-  if (random) {
-    return randomPoints().sort(byDistanceFromOrigin);
-  }
-  return staticPoints().sort(byDistanceFromOrigin);
+class PointsFactory {
 
-  function staticPoints() {
-    return [
+  static staticPoints() {
+    const result = [
       new Point(1, -1, 4),
       new Point(2, 6, -2),
       new Point(3, -3, 6),
@@ -18,24 +14,25 @@ function pointsFactory(random = false) {
       new Point(8, -10, -1),
       new Point(9, 5, -9),
       new Point(10, 7, 9),
-    ];
+    ]
+    return result.sort(this.byDistanceFromOrigin)
   }
-  function randomPoints() {
+  static randomPoints() {
     let result = [];
     for (let i = 0; i < 10; i++) {
       const id = i + 1;
       result.push(new Point(id, randomNumber(), randomNumber()));
     }
-    return result;
+    return result.sort(this.byDistanceFromOrigin);
   }
-  function byDistanceFromOrigin(a, b) {
+  byDistanceFromOrigin(a, b) {
     return a.hypotenuse - b.hypotenuse;
   }
-  function randomNumber() {
+  randomNumber() {
     let min = -10;
     let max = 10;
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 }
 
-export default pointsFactory;
+export default PointsFactory;
--- nearest-neighbour/index.js
+++ nearest-neighbour/index.js
@@ -1,21 +1,28 @@
 import Point from "./Point.js";
-import pointsFactory from "./pointsFactory.js";
-
-const points = pointsFactory();
+import PointsFactory from "./PointsFactory.js";
+ 
+const points = PointsFactory.staticPoints();
 const startingPoint = points[0];
 const processedIds = [startingPoint.id];
+const orderedPoints = []
 nearestNeighbourOf(startingPoint);
+console.log(orderedPoints.map(p => p.id).join(", "));
+
 
 function nearestNeighbourOf(theStartingPoint) {
   let nearestNeighbour = theStartingPoint;
   let distanceAcc = 9999;
 
   points.filter(excludeStartingPoint).forEach((p) => {
-    if (distanceAcc > temporaryPoint().dist) {
-      distanceAcc = temporaryPoint().dist;
-      nearestNeighbour = temporaryPoint();
+    findNeighbour()
+    
+    function findNeighbour() {
+      const temp = temporaryPoint();
+      if (distanceAcc > temp.dist) {
+        distanceAcc = temp.dist;
+        nearestNeighbour = temp;
+      }
     }
-
     function temporaryPoint() {
       const intermediateX = theStartingPoint.x - p.x;
       const intermediateY = theStartingPoint.y - p.y;
@@ -28,6 +35,7 @@ function nearestNeighbourOf(theStartingPoint) {
     `The neighbour of ${theStartingPoint.id} is ${nearestNeighbour.id}`
   );
 
+  orderedPoints.push(theStartingPoint)
   if (processedIds.length < 10) {
     const newStartingPoint = points.find((x) => x.id == nearestNeighbour.id);
     nearestNeighbourOf(newStartingPoint);
