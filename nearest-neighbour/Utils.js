0b8e6fe57617a329c410419ee93e18799f13f176
Diego Barbaresco <77250792+TooMuchLagWillKillYou@users.noreply.github.com>
2024-08-06 09:23:57 +0000

    moved orderPointsByDistance function

--- nearest-neighbour/Utils.js
+++ nearest-neighbour/Utils.js
@@ -1,3 +1,5 @@
+import Point from "./Point.js";
+
 function byDistanceFromOrigin(a, b) {
   return a.hypotenuse - b.hypotenuse;
 }
@@ -6,5 +8,37 @@ function randomNumber() {
   let max = 10;
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+function orderPointsByDistance(points, startingPointId = 1) {
 
-export { byDistanceFromOrigin, randomNumber };
+  const result = [];
+  nearestNeighbourOf(points.find(p => p.id == startingPointId));
+  
+  function nearestNeighbourOf(thisPoint) {
+    let nearestNeighbour = thisPoint;
+    let distanceAccumulator = 9999;
+    points.filter(excludeProcessedPoints).forEach(findNeighbourOf);
+    if (result.length < 10) {
+      result.push(thisPoint.id);
+      const newStartingPoint = points.find((x) => x.id == nearestNeighbour.id);
+      nearestNeighbourOf(newStartingPoint);
+    }
+    
+    //#region functions
+    function findNeighbourOf(aPoint) {
+      const temp = new Point(aPoint.id, thisPoint.x - aPoint.x, thisPoint.y - aPoint.y)
+      if (distanceAccumulator > temp.dist) {
+        distanceAccumulator = temp.dist;
+        nearestNeighbour = temp;
+      }
+    }
+    function excludeProcessedPoints(aPoint) {
+      return (
+        aPoint.id != thisPoint.id && !result.includes(aPoint.id)
+      );
+    }
+    //#endregion
+  }
+  return result
+}
+
+export { byDistanceFromOrigin, randomNumber, orderPointsByDistance };
--- nearest-neighbour/index.js
+++ nearest-neighbour/index.js
@@ -1,44 +1,40 @@
-import Point from "./Point.js";
+
 import PointsFactory from "./PointsFactory.js";
+import { orderPointsByDistance } from "./Utils.js";
 
-const points = new PointsFactory(false).points;
-const processedIds = [];
-nearestNeighbourOf(points[0]);
-console.log(processedIds.join(", "));
+const staticPoints = new PointsFactory(false).points;
+const ordereStaticPoints = orderPointsByDistance(staticPoints)
+console.log(ordereStaticPoints.join("-"));
 
-function nearestNeighbourOf(theStartingPoint) {
-  let nearestNeighbour = theStartingPoint;
-  let distanceAcc = 9999;
+// function orderPointsByDistance(points, startingPointId = 1) {
 
-  points.filter(excludeProcessedPoints).forEach(findNeighbourOf);
-
-  if (processedIds.length < 10) {
-    processedIds.push(theStartingPoint.id);
-    const newStartingPoint = points.find((x) => x.id == nearestNeighbour.id);
-    nearestNeighbourOf(newStartingPoint);
-  }
-
-  function findNeighbourOf(aPoint) {
-    const temp = temporaryPoint(aPoint);
-    if (distanceAcc > temp.dist) {
-      distanceAcc = temp.dist;
-      nearestNeighbour = temp;
-    }
-  }
-  function temporaryPoint(aPoint) {
-    const intermediateX = theStartingPoint.x - aPoint.x;
-    const intermediateY = theStartingPoint.y - aPoint.y;
-    return new Point(aPoint.id, intermediateX, intermediateY);
-  }
-  function excludeProcessedPoints(aPoint) {
-    return (
-      aPoint.id != theStartingPoint.id && !processedIds.includes(aPoint.id)
-    );
-  }
-}
-
-export default nearestNeighbourOf;
-
-// console.log(
-//   `The neighbour of ${theStartingPoint.id} is ${nearestNeighbour.id}`
-// );
+//   const result = [];
+//   nearestNeighbourOf(points.find(p => p.id == startingPointId));
+  
+//   function nearestNeighbourOf(thisPoint) {
+//     let nearestNeighbour = thisPoint;
+//     let distanceAccumulator = 9999;
+//     points.filter(excludeProcessedPoints).forEach(findNeighbourOf);
+//     if (result.length < 10) {
+//       result.push(thisPoint.id);
+//       const newStartingPoint = points.find((x) => x.id == nearestNeighbour.id);
+//       nearestNeighbourOf(newStartingPoint);
+//     }
+    
+//     //#region functions
+//     function findNeighbourOf(aPoint) {
+//       const temp = new Point(aPoint.id, thisPoint.x - aPoint.x, thisPoint.y - aPoint.y)
+//       if (distanceAccumulator > temp.dist) {
+//         distanceAccumulator = temp.dist;
+//         nearestNeighbour = temp;
+//       }
+//     }
+//     function excludeProcessedPoints(aPoint) {
+//       return (
+//         aPoint.id != thisPoint.id && !result.includes(aPoint.id)
+//       );
+//     }
+//     //#endregion
+//   }
+//   return result
+// }
